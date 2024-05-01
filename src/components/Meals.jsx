3877f351e48a73ccb1e806292e6ebf5ddf63e593
Grace Chen Abudi <98914366+Chen-Abudi@users.noreply.github.com>
2024-05-01 15:31:25 +0300

    Feature/http hook (#12)
    * feat: Add a custom http hook
    
    * feat: Handle http loading and error states

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,23 +1,41 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import MealItem from "./MealItem.jsx";
+import useHttp from "../hooks/useHttp.js";
+import Error from "./Error.jsx";
+
+const requestConfig = {};
 
 function Meals() {
-  const [loadedMeals, setLoadedMeals] = useState([]);
+  const {
+    data: loadedMeals,
+    isLoading,
+    error,
+  } = useHttp("http://localhost:3000/meals", requestConfig, []);
 
-  useEffect(() => {
-    async function fetchMeals() {
-      const response = await fetch("http://localhost:3000/meals");
+  if (isLoading) {
+    return <p className="center">Fetching Meals...</p>;
+  }
 
-      if (!response.ok) {
-        // Todo: Add an error handler ...
-      }
+  if (error) {
+    return <Error title="Failed to fetch meals" message={error} />;
+  }
 
-      const meals = await response.json();
-      setLoadedMeals(meals);
-    }
+  // const [loadedMeals, setLoadedMeals] = useState([]);
 
-    fetchMeals();
-  }, []);
+  // useEffect(() => {
+  //   async function fetchMeals() {
+  //     const response = await fetch("http://localhost:3000/meals");
+
+  //     if (!response.ok) {
+  //       // Todo: Add an error handler ...
+  //     }
+
+  //     const meals = await response.json();
+  //     setLoadedMeals(meals);
+  //   }
+
+  //   fetchMeals();
+  // }, []);
 
   return (
     <ul id="meals">
